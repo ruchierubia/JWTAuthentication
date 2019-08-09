516ab6c5e0ca06d0b6d98a073ee21cd49491827b
ruchierubia <ruch.rubi@gmail.com>
2019-08-10 01:24:47 +0800

    Add client client login view models and configure appsettings

---
+++ ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTAuthentication.ViewModels
+{
+    /// <summary>
+    /// View model for login from client app or postman
+    /// </summary>
+    public class LoginViewModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ ViewModels/RegisterViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTAuthentication.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public int Email { get; set; }
+
+        [Required]
+        public int Password { get; set; }
+    }
+}
